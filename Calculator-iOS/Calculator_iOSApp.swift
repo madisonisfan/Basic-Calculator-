d31256a536a5a731d7d7c376854420064b3ac381
Madison Isfan <isfanmadison@gmail.com>
2022-10-26 15:51:45 -0700

    Initial Commit

---
+++ Calculator-iOS/Calculator_iOSApp.swift
@@ -0,0 +1,20 @@
+//
+//  Calculator_iOSApp.swift
+//  Calculator-iOS
+//
+//  Created by Madison Isfan on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_iOSApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
